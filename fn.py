562139bbf9c6a3bee5af7038efca42b8ef3a1ea0
akashsingh <akashsingh@propelld.com>
2024-01-22 03:48:49 +0530

    Added

--- fn.py
+++ fn.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from datetime import datetime
+import pymongo
+import pandas as pd
+
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb+srv://akash9936:Tree9936@cluster0.f1wthph.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+collection = db.nse50
 
 def main():
+    st.write()
     st.title("Trying to get insider candle, dekh le ab kya karna h:")
 
     # Add "from" date and time input fields
@@ -25,11 +34,93 @@ def main():
 
     if analyze_button:
         # Call a function to analyze data and update the HTML content
-        analyze_data()
+        analyze_data(from_date, from_time, to_date, to_time)
 
-def analyze_data():
-    # Your data analysis logic goes here
-    pass
+def analyze_data(from_date, from_time, to_date, to_time):
+    try:
+        format_str = '%d-%b-%Y %H:%M:%S'
+        from_datetime = datetime.combine(from_date, from_time)
+        to_datetime = datetime.combine(to_date, to_time)
+
+        total_data = 30
+        slice_data = total_data // 2
+
+        half_time = (from_datetime + (to_datetime - from_datetime) // 2).strftime(format_str)
+
+        print(f"from_datetime1: {from_datetime.strftime(format_str)}")
+        print(f"to_datetime: {to_datetime.strftime(format_str)}")
+        print(f"half_time: {half_time}")
+
+        last_records_cursor = collection.find({
+            'timestamp': {'$gte': half_time, '$lt': to_datetime.strftime(format_str)}
+        }).sort('_id', pymongo.DESCENDING).limit(slice_data)
+        last_records = list(last_records_cursor)
+
+        if not last_records:
+            st.error('Error: No data found in lastRecords MongoDB.')
+        else:
+            print("last_records found")
+
+        previous_records_cursor = collection.find({
+            'timestamp': {'$gte': from_datetime.strftime(format_str), '$lt': half_time}
+        }).sort('_id', pymongo.DESCENDING).limit(slice_data)
+
+        previous_records = list(previous_records_cursor)
+
+        if not previous_records:
+            st.error('Error: No data found in previousRecords MongoDB.')
+        else:
+            print("Previous records found")
+          #  for record in previous_records:
+          #      print(record)
+
+        symbols_last_prices_last = []
+        symbols_last_prices_previous = []
+
+        for entry in last_records:
+            for stock in entry['data']:
+                symbols_last_prices_last.append({
+                    'symbol': stock['symbol'],
+                    'lastPrice': stock['lastPrice'],
+                    'timestamp': entry['timestamp'],
+                })
+
+        for entry in previous_records:
+            for stock in entry['data']:
+                symbols_last_prices_previous.append({
+                    'symbol': stock['symbol'],
+                    'lastPrice': stock['lastPrice'],
+                    'timestamp': entry['timestamp'],
+                })
+
+        symbols_data = pd.DataFrame(symbols_last_prices_last).groupby('symbol').agg({
+            'lastPrice': ['max', 'min', 'first', 'last'],
+            'timestamp': ['first', 'last']
+        }).reset_index()
+
+        symbols_data.columns = ['symbol', 'max_last_price_last', 'min_last_price_last',
+                                'first_last_price_last', 'last_last_price_last',
+                                'first_timestamp_last', 'last_timestamp_last']
+
+        symbols_data_previous = pd.DataFrame(symbols_last_prices_previous).groupby('symbol').agg({
+            'lastPrice': ['max', 'min', 'first', 'last'],
+            'timestamp': ['first', 'last']
+        }).reset_index()
+
+        symbols_data_previous.columns = ['symbol', 'max_last_price_previous', 'min_last_price_previous',
+                                         'first_last_price_previous', 'last_last_price_previous',
+                                         'first_timestamp_previous', 'last_timestamp_previous']
+
+        symbols_data = pd.merge(symbols_data, symbols_data_previous, on='symbol', how='inner')
+
+        symbols_data['buy_signal'] = (symbols_data['min_last_price_last'] > symbols_data['min_last_price_previous']) & \
+                                    (symbols_data['max_last_price_last'] < symbols_data['max_last_price_previous'])
+
+        st.dataframe(symbols_data)
+        
+        st.success("Data analysis successful!")
+    except Exception as e:
+        st.error(f"Error fetching data: {str(e)}")
 
 if __name__ == "__main__":
     main()
